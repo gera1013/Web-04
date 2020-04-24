70f209438b8cb3df599704d61806f47b8d3eb8ed
gera1013 <gera1013@users.noreply.github.com>
2020-04-24 02:21:40 -0600

    4to Commit - Permisos a Eventos

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
 from guardian.shortcuts import assign_perm
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from permissions.services import APIPermissionClassFactory
 from events.models import Event
@@ -23,9 +24,16 @@ class EventViewSet(viewsets.ModelViewSet):
                 'instance': {
                     'retrieve': evaluar,
                     'destroy': evaluar,
-                    'update': evaluar,
-                    'partial_update': evaluar,
+                    'update': 'events.change_event',
+                    'partial_update': 'events.change_event',
                 }
             }
         ),
     )
+
+    def perform_create(self, serializer):
+        event = serializer.save()
+        user = self.request.user
+        assign_perm('events.view_event', user, event)
+        assign_perm('events.change_event', user, event)
+        return Response(serializer.data)
